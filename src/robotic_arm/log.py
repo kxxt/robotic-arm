005dcd8460d9d1cdb40e9df2c531e8cef648a0e6
kxxt <18085551+kxxt@users.noreply.github.com>
2021-04-10 11:56:00 +0800

    Disable some 3-rd party logs.

--- src/robotic_arm/log.py
+++ src/robotic_arm/log.py
@@ -1,10 +1,17 @@
 from robotic_arm.config import LOGGING_LEVEL
 
+
 def init_logger():
     import logging
 
     logger = logging.getLogger()
     logger.setLevel(level=LOGGING_LEVEL)
+
+    # BEGIN Disable some logs from 3-rd party libraries
+    com_logger = logging.getLogger("comtypes")
+    com_logger.setLevel(logging.WARNING)
+    # END Disable some logs from 3-rd party libraries
+
     file_handler = logging.FileHandler("robotic-arm.log")
     file_handler.setLevel(LOGGING_LEVEL)
     console_handler = logging.StreamHandler()
