d1237210f6ce92bac3c406a51745438cd9469a84
kxxt <18085551+kxxt@users.noreply.github.com>
2021-04-13 20:44:49 +0800

    A theoretically correct implementation of face recognition.

--- src/robotic_arm/recognition/face.py
+++ src/robotic_arm/recognition/face.py
@@ -1,15 +1,48 @@
 from robotic_arm.recognition.base import ImageRecognitionService
 import logging
+import numpy as np
+from robotic_arm.input.camera import get_frame
+from datetime import datetime
 
 
 class FaceRecognitionService(ImageRecognitionService):
     def __init__(self):
         ImageRecognitionService.__init__(self, 'face-recognition')
         self.logger = logging.getLogger(__name__)
+        self.service = None
+        self.process_this_frame = True
 
     def load(self):
-        from time import sleep
-        sleep(5)
+        self.service = __import__("face_recognition")
 
-    def recognize(self, img):
-        pass
+    known_face_encodings = []
+    known_face_names = []
+
+    def recognize(self, frame):
+        if self.process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = self.service.face_locations(frame)
+            face_encodings = self.service.face_encodings(frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = self.service.compare_faces(self.known_face_encodings, face_encoding)
+                name = "Unknown"
+                # # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = self.service.face_distance(self.known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = self.known_face_names[best_match_index]
+
+                face_names.append(name)
+            self.output_queue.put((face_names, face_locations, face_encodings))
+        self.process_this_frame = not self.process_this_frame
+
+    def real_work(self):
+        self.output_queue.put(self.recognize(get_frame()))
