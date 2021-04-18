c160d7bfc0b941872273cfb9a407dc593a42c5c6
kxxt <18085551+kxxt@users.noreply.github.com>
2021-04-18 11:12:19 +0800

    Add pydub impl.

---
+++ src/robotic_arm/output/voice_pydub_impl.py
@@ -0,0 +1,36 @@
+import logging
+from pydub import AudioSegment
+from pydub.playback import play
+from robotic_arm.output.static_voice_preprocessor import get, get_dict, load as preload
+import threading
+from queue import Queue
+
+logger = logging.getLogger("pydub")
+voice_cache = dict()
+q = Queue()
+
+
+def utter(text: str):
+    play(voice_cache[text])
+
+
+def utter_async(text: str):
+    q.put(text)
+
+
+def work():
+    while True:
+        utter(q.get(block=True))
+
+
+thread = threading.Thread(target=work, name="pydub-queue", daemon=True)
+
+
+def load():
+    global voice_cache
+    preload()
+    # logger.debug(sv_dict)
+    logger.info("Loading voice composition cache into memory...")
+    voice_cache = {key: AudioSegment.from_wav(get(key)) for key in get_dict()}
+    logger.debug(voice_cache)
+    thread.start()
