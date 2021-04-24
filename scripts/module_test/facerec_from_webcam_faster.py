fb38544b37484433a86189edcd18801fc5e27f4e
kxxt <18085551+kxxt@users.noreply.github.com>
2021-04-24 23:04:05 +0800

    Process 1 frame per 3 frames.

--- scripts/module_test/facerec_from_webcam_faster.py
+++ scripts/module_test/facerec_from_webcam_faster.py
@@ -36,7 +36,7 @@ known_face_names = [
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
+process = 0
 
 while True:
     # Grab a single frame of video
@@ -49,7 +49,7 @@ while True:
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if process_this_frame:
+    if process % 3 == 0:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -57,7 +57,7 @@ while True:
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            ##matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -66,22 +66,21 @@ while True:
             #     name = known_face_names[first_match_index]
 
             # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+            ##face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            ##best_match_index = np.argmin(face_distances)
+            ##if matches[best_match_index]:
+            ##    name = known_face_names[best_match_index]
 
             face_names.append(name)
 
-    process_this_frame = not process_this_frame
-
+    process += 1
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 5
         right *= 5
-        bottom *=5
+        bottom *= 5
         left *= 5
 
         # Draw a box around the face
