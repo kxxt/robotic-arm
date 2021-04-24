a030c673bf155b7a9c1437c2d629a326afbb67e2
kxxt <18085551+kxxt@users.noreply.github.com>
2021-04-24 23:09:28 +0800

    Format config.py and add more configuration options.

--- src/robotic_arm/config.py
+++ src/robotic_arm/config.py
@@ -3,30 +3,50 @@ from os import path, environ
 import sounddevice as sd
 import logging
 
+# Basic profile data
 NAME = '机器人'
 
+# Hardware configurations
 ARM_LENGTHS = []
 SERVO_PINS = []
+
+# Electronic configurations
 LIGHT_PIN = environ.get('LIGHT_PIN') or 14
 
-VOICE_RECOGNITION_MODEL_PATH = environ.get('VOICE_RECOGNITION_MODEL_PATH') \
-                               or path.join("models", "voice_recognition")
-
-VOICE_DEVICE = environ.get('VOICE_DEVICE')
-
-VOICE_SAMPLE_RATE = environ.get('VOICE_SAMPLE_RATE') \
-                    or int(sd.query_devices(VOICE_DEVICE, 'input')['default_samplerate'])
-
-VOICE_CHANNELS = environ.get('VOICE_CHANNELS') or 1
-
-VOICE_BLOCKSIZE = environ.get('VOICE_BLOCKSIZE') or 8000
-
+# Application configurations
 LOGGING_LEVEL = logging.DEBUG
 
+# Camera configurations
 CAMERA_ID = environ.get('CAMERA_ID') or 0
+
+# Voice composition configurations
 
 # available solutions are pydub, playsound, pyttsx3
 VOICE_COMPOSE_SOLUTION = environ.get('VOICE_COMPOSE_SOLUTION') or "pydub"
-
 VOICE_JSON_PATH = environ.get('VOICE_JSON_PATH') or "audio.json"
 USE_PROCESSED_VOICE_FILE = environ.get('USE_PROCESSED_VOICE_FILE') or False
+
+# face recognition configurations
+
+# available solutions are face_recognition, mediapipe
+FACE_RECOGNITION_SOLUTION = environ.get('FACE_RECOGNITION_SOLUTION') or "mediapipe"
+
+# Mediapipe face_detection configurations.
+MEDIAPIPE_FACE_DETECTION_MIN_CONFIDENCE = environ.get('MEDIAPIPE_FACE_DETECTION_MIN_CONFIDENCE') or 0.5
+
+# hand tracking configurations
+HANDS_MIN_DETECTION_CONFIDENCE = environ.get('HANDS_MIN_DETECTION_CONFIDENCE') or 0.5
+HANDS_MIN_TRACKING_CONFIDENCE = environ.get('HANDS_MIN_TRACKING_CONFIDENCE') or 0.5
+
+# Voice recognition configurations
+VOICE_RECOGNITION_MODEL_PATH = environ.get('VOICE_RECOGNITION_MODEL_PATH') \
+                               or path.join("models", "voice_recognition")
+VOICE_DEVICE = environ.get('VOICE_DEVICE')
+VOICE_SAMPLE_RATE = environ.get('VOICE_SAMPLE_RATE') \
+                    or int(sd.query_devices(VOICE_DEVICE, 'input')['default_samplerate'])
+VOICE_CHANNELS = environ.get('VOICE_CHANNELS') or 1
+VOICE_BLOCKSIZE = environ.get('VOICE_BLOCKSIZE') or 8000
+
+# Lazy configurations
+__load_real_services = environ.get('LOAD_REAL_SERVICES')
+LOAD_REAL_SERVICES = True if __load_real_services is None else __load_real_services.lower() != "false"
