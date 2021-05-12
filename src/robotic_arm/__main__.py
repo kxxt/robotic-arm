0975abb31c34bc627025987a107861726d8adb5f
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-12 20:45:38 +0800

    Inject motion functionality into RoboticArm. (Include a work-around)

--- src/robotic_arm/__main__.py
+++ src/robotic_arm/__main__.py
@@ -1,11 +1,26 @@
+import logging
+import os
+
 from robotic_arm.output import load_voice_composition_service
+
 load_voice_composition_service()
 
 from robotic_arm import RoboticArm
-import logging
 from robotic_arm.recognition import *
+from robotic_arm.motion import Motion
 
 logger = logging.getLogger(__name__)
+
+motion = Motion()
+
+# WORK-AROUND FOR SIMULATION
+# 因为模拟在运行时会把当前目录切到磁盘根目录
+# 影响其他服务的加载
+# 所以这里有一个 UGLY WORK-AROUND
+
+work_dir = os.getcwd()
+motion.load()
+os.chdir(work_dir)
 
 face_service = FaceRecognitionService()
 hands_service = HandsRecognitionService()
@@ -14,5 +29,5 @@ face_service.run()
 hands_service.run()
 voice_service.run()
 
-arm = RoboticArm(face_service, voice_service, hands_service)
+arm = RoboticArm(face_service, voice_service, hands_service, motion)
 arm.run()
