54f2584e00026bf7aaee1abf650112a9d9f4198c
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-12 20:40:58 +0800

    Add simulated motion implementation.

---
+++ src/robotic_arm/motion/simulation/robot.py
@@ -0,0 +1,19 @@
+import roboticstoolbox as rtb
+from roboticstoolbox.backends.swift import Swift
+from robotic_arm.motion.base import MotionBase
+
+
+class SimulatedMotion(MotionBase):
+    def __init__(self):
+        self.robot = None
+        self.backend = None
+
+    def load(self):
+        self.robot = rtb.models.Panda()
+        self.backend = Swift()
+        self.backend.launch()
+        self.backend.add(self.robot)
+
+    def set(self, servo_id, value):
+        self.robot.q[servo_id] = value
+        self.backend.step()
