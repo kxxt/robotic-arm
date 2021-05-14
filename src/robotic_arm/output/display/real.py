1b8ecb53d7c12338b6501088417e6a17c03d7f69
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-14 23:33:57 +0800

    Add device initialization method for real and simulated devices.

---
+++ src/robotic_arm/output/display/real.py
@@ -0,0 +1,11 @@
+from robotic_arm.config import OLED_DISPLAY_DRIVER, OLED_DISPLAY_INTERFACE, OLED_DISPLAY_WIDTH, OLED_DISPLAY_HEIGHT
+from robotic_arm.output.display.device import get_device
+
+
+def init_device_from_config():
+    return get_device([
+        f"--display {OLED_DISPLAY_DRIVER}",
+        f"--interface {OLED_DISPLAY_INTERFACE}",
+        f"--width {OLED_DISPLAY_WIDTH}",
+        f"--height {OLED_DISPLAY_HEIGHT}"
+    ])
