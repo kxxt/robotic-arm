a273e8b6b4e150a11b5b593b614a6563aad9f5e7
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-15 17:30:05 +0800

    Export load_display_service and drawing.

--- src/robotic_arm/output/__init__.py
+++ src/robotic_arm/output/__init__.py
@@ -1,2 +1,4 @@
 from robotic_arm.output.voice import utter, utter_async, load_voice_composition_service
 from robotic_arm.output.light import Light
+from robotic_arm.output.display import load_display_service
+from robotic_arm.output.display import drawing
