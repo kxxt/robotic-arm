277b8fd315313174ea69d8e53bb6bea544fc43a7
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-15 17:20:23 +0800

    Refactor and add the method to load the display service。

--- src/robotic_arm/output/display/__init__.py
+++ src/robotic_arm/output/display/__init__.py
@@ -1,4 +1,16 @@
 from robotic_arm.config import OLED_DISPLAY_IMPLEMENTATION
-from robotic_arm.output.display import real,simulated
 
-oled = real.init_device_from_config() if OLED_DISPLAY_IMPLEMENTATION == 'real' else simulated.init_device_from_config()
+if OLED_DISPLAY_IMPLEMENTATION == 'real':
+    from robotic_arm.output.display.real import init_device_from_config
+elif OLED_DISPLAY_IMPLEMENTATION == 'simulated':
+    from robotic_arm.output.display.simulated import init_device_from_config
+else:
+    def init_device_from_config(args):
+        pass
+
+
+def load_display_service():
+    from robotic_arm.output.display.display import Display
+    d = Display(init_device_from_config())
+    d.start()
+    return d
