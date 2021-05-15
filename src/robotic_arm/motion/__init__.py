5d1b1ac21161a9d30da451985fb1a1fae7e3e5fb
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-15 09:33:13 +0800

    Use multiprocessing simulation by default.

--- src/robotic_arm/motion/__init__.py
+++ src/robotic_arm/motion/__init__.py
@@ -1,7 +1,7 @@
 from robotic_arm.config import SERVO_IMPLEMENTATION
 
 if SERVO_IMPLEMENTATION == 'simulated':
-    from robotic_arm.motion.simulation import SimulatedMotion as Motion
+    from robotic_arm.motion.simulation import MultiprocessingSimulatedMotion as Motion
 elif SERVO_IMPLEMENTATION == 'real':
     from robotic_arm.motion.reality import RealMotion as Motion
 else:
