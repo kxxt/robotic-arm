d361b760251ee4918179b6fcf86fab0dceff288c
kxxt <18085551+kxxt@users.noreply.github.com>
2021-05-15 09:31:22 +0800

    Bug fixes.

--- src/robotic_arm/output/display/real.py
+++ src/robotic_arm/output/display/real.py
@@ -4,8 +4,8 @@ from robotic_arm.output.display.device import get_device
 
 def init_device_from_config():
     return get_device([
-        f"--display {OLED_DISPLAY_DRIVER}",
-        f"--interface {OLED_DISPLAY_INTERFACE}",
-        f"--width {OLED_DISPLAY_WIDTH}",
-        f"--height {OLED_DISPLAY_HEIGHT}"
+        f"--display={OLED_DISPLAY_DRIVER}",
+        f"--interface={OLED_DISPLAY_INTERFACE}",
+        f"--width={OLED_DISPLAY_WIDTH}",
+        f"--height={OLED_DISPLAY_HEIGHT}"
     ])
--- src/robotic_arm/output/display/simulated.py
+++ src/robotic_arm/output/display/simulated.py
@@ -4,7 +4,7 @@ from robotic_arm.output.display.device import get_device
 
 def init_device_from_config():
     return get_device([
-        "--display pygame",
-        f"--width {OLED_DISPLAY_WIDTH}",
-        f"--height {OLED_DISPLAY_HEIGHT}"
+        "--display=pygame",
+        f"--width={OLED_DISPLAY_WIDTH}",
+        f"--height={OLED_DISPLAY_HEIGHT}"
     ])
