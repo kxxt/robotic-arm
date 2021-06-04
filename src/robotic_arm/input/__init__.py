7a9596b2301c1c947867ccba07fa000835437707
kxxt <18085551+kxxt@users.noreply.github.com>
2021-06-04 18:42:36 +0800

    Implement async loading of camera.

---
+++ src/robotic_arm/input/__init__.py
@@ -0,0 +1,1 @@
+from robotic_arm.input.camera import is_video_ready, init_video_device_async
--- src/robotic_arm/input/camera.py
+++ src/robotic_arm/input/camera.py
@@ -1,7 +1,29 @@
-import cv2
-from robotic_arm.config import CAMERA_ID
+from typing import Union
 
-video_capture = cv2.VideoCapture(CAMERA_ID)
+import cv2
+from cv2.cv2 import VideoCapture
+
+from robotic_arm.config import CAMERA_ID
+import threading
+
+video_capture: Union[VideoCapture, None] = None
+video_ready: threading.Event = threading.Event()
+
+
+def is_video_ready():
+    return video_ready.is_set()
+
+
+def init_video_device():
+    global video_capture, video_ready
+    cv2.VideoCapture(CAMERA_ID)
+    video_ready.set()
+
+
+def init_video_device_async():
+    global video_capture
+    thread = threading.Thread(target=init_video_device, name="init-video", daemon=True)
+    thread.start()
 
 
 def get_raw_frame():
