0d556caef0b096fee0d5d884a3680e28f4b25847
kxxt <18085551+kxxt@users.noreply.github.com>
2021-06-04 20:31:14 +0800

    Add method to test robot.

--- src/robotic_arm/motion/simulation/robot.py
+++ src/robotic_arm/motion/simulation/robot.py
@@ -19,6 +19,19 @@ class SimulatedMotion(MotionBase):
         self.backend.launch()
         self.backend.add(self.robot)
 
+    def for_test_purpose(self):
+        import spatialmath as sm
+        import numpy as np
+        Tep = self.robot.fkine(self.robot.q) * sm.SE3.Tx(0.2) * sm.SE3.Ty(0.2) * sm.SE3.Tz(0.45)
+        arrived = False
+        while not arrived:
+            # Work out the required end-effector velocity to go towards the goal
+            v, arrived = rtb.p_servo(self.robot.fkine(self.robot.q), Tep, 1)
+            # Set the self.robot's joint velocities
+            self.robot.qd = np.linalg.pinv(self.robot.jacobe(self.robot.q)) @ v
+            # Step the simulator by 50 milliseconds
+            self.backend.step(0.05)
+
     def set(self, servo_id, value):
         self.robot.q[servo_id] = value
         self.backend.step()
