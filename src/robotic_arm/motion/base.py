b0638ff75eea9635a286de7906431318f260dfe4
kxxt <rsworktech@outlook.com>
2021-07-07 15:33:54 +0800

    Add servo base class.

--- src/robotic_arm/motion/base.py
+++ src/robotic_arm/motion/base.py
@@ -7,3 +7,14 @@ class MotionBase:
 
     def set_all(self, value):
         pass
+
+
+class ServoBase:
+    def load(self):
+        pass
+
+    def set(self, value):
+        pass
+
+    def exec(self, cmd, param):
+        pass
