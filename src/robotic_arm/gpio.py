552748b4ce28ea2e8c1244c0ad02cd3ed6dfa9a8
kxxt <rsworktech@outlook.com>
2021-07-07 23:28:04 +0800

    Add fake gpio for windows.

--- src/robotic_arm/gpio.py
+++ src/robotic_arm/gpio.py
@@ -1,3 +1,21 @@
-import pigpio
+try:
+    import pigpio
 
-pi = pigpio.pi()
+    pi = pigpio.pi()
+except ImportError:
+    import logging
+
+    logger = logging.getLogger("GPIO")
+    logger.error("Failed to import GPIO library: pigpio!")
+    logger.error("Ignore this error if you are on other platforms, but all gpio functions won't work at all!")
+
+
+    class FakeGPIO:
+        def __getattr__(self, name):
+            return self
+
+        def __call__(self, *args, **kwargs):
+            return
+
+
+    pi = FakeGPIO()
