c5285badc2952f2b4def62029f1ecda95fbbde14
kxxt <rsworktech@outlook.com>
2021-07-07 20:54:31 +0800

    Add time param for Motion/ServoBase set/set_all method.

--- src/robotic_arm/motion/base.py
+++ src/robotic_arm/motion/base.py
@@ -2,10 +2,16 @@ class MotionBase:
     def load(self):
         pass
 
-    def set(self, servo_id, value):
+    def set(self, servo_id, value, time=1000):
         pass
 
-    def set_all(self, value):
+    def set_all(self, value, time=1000):
+        pass
+
+    def get(self, servo_id):
+        pass
+
+    def get_all(self):
         pass
 
 
@@ -13,7 +19,10 @@ class ServoBase:
     def load(self):
         pass
 
-    def set(self, value):
+    def set(self, value, time=1000):
+        pass
+
+    def get(self):
         pass
 
     def exec(self, cmd, param):
