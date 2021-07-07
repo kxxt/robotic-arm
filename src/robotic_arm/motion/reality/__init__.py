8460dd5b372fe381d092a8ee11e69e2c14882808
kxxt <rsworktech@outlook.com>
2021-07-07 22:15:51 +0800

    Export *Servo(s)/Motion class to __init__.py

--- src/robotic_arm/motion/reality/__init__.py
+++ src/robotic_arm/motion/reality/__init__.py
@@ -1,1 +1,4 @@
 from robotic_arm.motion.reality.servo import RealMotion
+from robotic_arm.motion.reality.digital import DigitalServo
+from robotic_arm.motion.reality.serial import SerialServos
+from robotic_arm.motion.reality.composite import CompositeMotion
