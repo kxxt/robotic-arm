9e11d85efaf9027bca67a365b0fccc6f751ad507
kxxt <rsworktech@outlook.com>
2021-07-09 07:09:24 +0800

    Use physics field instead of robot field.

--- src/robotic_arm/motion/simulation/robot.py
+++ src/robotic_arm/motion/simulation/robot.py
@@ -11,32 +11,32 @@ from robotic_arm.motion.physics import PhysicalModel
 
 class SimulatedMotion(MotionBase):
     def __init__(self):
-        self.robot = None
+        super().__init__()
         self.backend = None
 
     def load(self):
-        self.robot = PhysicalModel()
+        self.physics = PhysicalModel()
         self.backend = Swift()
         self.backend.launch()
-        self.backend.add(self.robot)
+        self.backend.add(self.physics)
 
     def for_test_purpose(self):
         import spatialmath as sm
         import numpy as np
-        Tep = self.robot.fkine(self.robot.q) * sm.SE3.Tx(0.2) * sm.SE3.Ty(0.2) * sm.SE3.Tz(0.45)
+        Tep = self.physics.fkine(self.physics.q) * sm.SE3.Tx(0.2) * sm.SE3.Ty(0.2) * sm.SE3.Tz(0.45)
         arrived = False
         while not arrived:
             # Work out the required end-effector velocity to go towards the goal
-            v, arrived = rtb.p_servo(self.robot.fkine(self.robot.q), Tep, 1)
-            # Set the self.robot's joint velocities
-            self.robot.qd = np.linalg.pinv(self.robot.jacobe(self.robot.q)) @ v
+            v, arrived = rtb.p_servo(self.physics.fkine(self.physics.q), Tep, 1)
+            # Set the self.physics's joint velocities
+            self.physics.qd = np.linalg.pinv(self.physics.jacobe(self.physics.q)) @ v
             # Step the simulator by 50 milliseconds
             self.backend.step(0.05)
 
     def set(self, servo_id, value, time=50):
-        self.robot.q[servo_id] = value
+        self.physics.q[servo_id] = value
         self.backend.step(time / 1000)
 
     def set_all(self, value, time=50):
-        self.robot.q = value
+        self.physics.q = value
         self.backend.step(50 / 1000)
