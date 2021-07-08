7b958b8b357a332167a64cbd431533d4e425d330
kxxt <rsworktech@outlook.com>
2021-07-09 07:53:35 +0800

    Impl LightSensor.

--- src/robotic_arm/input/sensors/light.py
+++ src/robotic_arm/input/sensors/light.py
@@ -1,6 +1,14 @@
 from robotic_arm.input.sensors.base import SensorBase
+from robotic_arm.gpio import pi
+from robotic_arm.config import LIGHTNESS_SENSOR_PIN
+
+import pigpio
 
 
 class LightSensor(SensorBase):
-    def init_sensor(self):
-        pass
+    def __init__(self, pin=None):
+        self.pin = pin or LIGHTNESS_SENSOR_PIN
+        pi.set_mode(self.pin, pigpio.INPUT)
+
+    def read(self):
+        return pi.read(self.pin)
