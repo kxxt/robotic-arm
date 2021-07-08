fd9ea4438a023f222c593b3734d83b0717bfce34
kxxt <rsworktech@outlook.com>
2021-07-09 07:53:18 +0800

    Impl Light.

--- src/robotic_arm/output/light.py
+++ src/robotic_arm/output/light.py
@@ -1,15 +1,20 @@
 from colour import Color
+import pigpio
+
+from robotic_arm.config import LIGHT_PIN
+from robotic_arm.gpio import pi
 
 
 class Light:
     def __init__(self, pin: int):
-        self.pin = pin
+        self.pin = pin or LIGHT_PIN
+        pi.set_mode(self.pin, pigpio.OUTPUT)
 
-    def switch(self, c: Color, state: bool):
-        pass
+    def switch(self, state: bool):
+        pi.write(self.pin, state)
 
-    def on(self, c: Color):
-        pass
+    def on(self):
+        pi.write(self.pin, 1)
 
     def off(self):
-        pass
+        pi.write(self.pin, 1)
