ee3eab9f289351c1e6294037b37a8622791a8262
kxxt <rsworktech@outlook.com>
2021-07-09 06:58:36 +0800

    Add method to implement a camera 2 world position transform.

--- src/robotic_arm/motion/physics.py
+++ src/robotic_arm/motion/physics.py
@@ -13,3 +13,12 @@ class PhysicalModel(ERobot):
         super().__init__(
             links, name=name, manufacturer="Robot"
         )
+
+    def camera_to_world(self, cpos, dis=1):
+        """
+
+        :param cpos: numpy ndarray , camera position.
+        :return: world position
+        """
+        return (self.links[5].A(self.q[4]) @ self.links[4].A(self.q[3]) @ self.links[3].A(self.q[2])
+                @ self.links[2].A(self.q[1]) @ self.links[1].A(self.q[0])).inv() @ np.append(cpos, [dis, 1])
