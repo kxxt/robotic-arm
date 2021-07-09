6fd64ebd4d1cbc0617806fd5a0a6a803840dcd97
kxxt <rsworktech@outlook.com>
2021-07-09 22:14:15 +0800

    Impl Flicker.

--- src/robotic_arm/output/light.py
+++ src/robotic_arm/output/light.py
@@ -1,13 +1,29 @@
-from colour import Color
-
+import threading
 from robotic_arm.config import LIGHT_PIN
 from robotic_arm.gpio import pi, pigpio
+from time import sleep
 
 
 class Light:
     def __init__(self, pin: int = None):
         self.pin = pin or LIGHT_PIN
+        self.thread = threading.Thread(target=self.working, daemon=True)
+        self.flicker = threading.Event()
         pi.set_mode(self.pin, pigpio.OUTPUT)
+
+    def start_flicker(self):
+        self.flicker.set()
+
+    def stop_flicker(self):
+        self.flicker.clear()
+
+    def working(self):
+        while True:
+            self.flicker.wait()
+            while self.flicker.is_set():
+                self.on()
+                sleep(0.5)
+                self.off()
 
     def switch(self, state: bool):
         pi.write(self.pin, state)
