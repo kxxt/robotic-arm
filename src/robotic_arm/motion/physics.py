08084c1b9e827c6d569783521da7a321bd8285c6
kxxt <rsworktech@outlook.com>
2021-07-09 12:36:59 +0800

    Fix camera_to_world. _move WIP...

--- src/robotic_arm/motion/physics.py
+++ src/robotic_arm/motion/physics.py
@@ -5,6 +5,7 @@ from roboticstoolbox import RevoluteDH, PrismaticDH
 import spatialmath as sm
 from math import pi
 from robotic_arm.config import URDF_PATH
+from robotic_arm.utils.judge import get_diff_vector_from_center
 
 
 class PhysicalModel(ERobot):
@@ -20,5 +21,17 @@ class PhysicalModel(ERobot):
         :param cpos: numpy ndarray , camera position.
         :return: world position
         """
-        return (self.links[5].A(self.q[4]) @ self.links[4].A(self.q[3]) @ self.links[3].A(self.q[2])
-                @ self.links[2].A(self.q[1]) @ self.links[1].A(self.q[0])).inv() @ np.append(cpos, [dis, 1])
+        return (self.links[1].A(self.q[0])
+                @ self.links[2].A(self.q[1])
+                @ self.links[3].A(self.q[2])
+                @ self.links[4].A(self.q[3])
+                @ self.links[5].A(self.q[4])
+                ) @ np.append(cpos, [dis, 1])
+
+    def _move(self, cdata):
+        vec = get_diff_vector_from_center(cdata)
+        wvec = self.camera_to_world(vec)
+        pose = self.fkine(self.q)
+        pose[:, -1] = wvec
+        return self.ikine_LM(pose)
+
