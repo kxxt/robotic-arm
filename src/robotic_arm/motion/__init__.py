dab030343ce797ae61f333eef5fb60fc351ad40a
kxxt <rsworktech@outlook.com>
2021-07-09 22:12:54 +0800

    Use composited Motion, remove real motion.

--- src/robotic_arm/motion/__init__.py
+++ src/robotic_arm/motion/__init__.py
@@ -3,6 +3,6 @@ from robotic_arm.config import SERVO_IMPLEMENTATION
 if SERVO_IMPLEMENTATION == 'simulated':
     from robotic_arm.motion.simulation import SimulatedMotion as Motion
 elif SERVO_IMPLEMENTATION == 'real':
-    from robotic_arm.motion.reality import RealMotion as Motion
+    from robotic_arm.motion.reality import CompositeMotion as Motion
 else:
     from robotic_arm.motion.base import MotionBase as Motion
--- src/robotic_arm/motion/reality/__init__.py
+++ src/robotic_arm/motion/reality/__init__.py
@@ -1,4 +1,3 @@
-from robotic_arm.motion.reality.servo import RealMotion
 from robotic_arm.motion.reality.digital import DigitalServo
 from robotic_arm.motion.reality.serial import SerialServos
 from robotic_arm.motion.reality.composite import CompositeMotion
